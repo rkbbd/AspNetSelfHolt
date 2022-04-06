4b2fd2ffeaee48d417810498cc0b01947290761a
rakib <rakib@email.com>
2022-04-06 14:47:00 +0600

    controller chg

--- SelfHosting/SelfHosting/HomeController.cs
+++ SelfHosting/SelfHosting/HomeController.cs
@@ -4,29 +4,29 @@ namespace SelfHosting
 {
     public class HomeController : ApiController
     {
-        // GET api/demo 
+        // GET api/home
         public IEnumerable<string> Get()
         {
             return new string[] { "Hello", "World" };
         }
 
-        // GET api/demo/5 
+        // GET api/home/5 
         public string Get(int id)
         {
             return "Hello, World!";
         }
 
-        // POST api/demo 
+        // POST api/home 
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/demo/5 
+        // PUT api/home/5 
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/demo/5 
+        // DELETE api/home/5 
         public void Delete(int id)
         {
         }
