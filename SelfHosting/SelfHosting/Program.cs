c93631a8c31c2205d0b68813619d2c6e54a48921
rakib.hasan@efficientsoftwaresolutions.com <rakib.hasan@efficientsoftwaresolutions.com>
2022-04-06 14:40:31 +0600

    initial

---
+++ SelfHosting/SelfHosting/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8080"))
+            {
+                Console.WriteLine("Web Server is running.");
+                Console.WriteLine("http://localhost:8080/api/home");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SelfHosting/SelfHosting/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(SelfHosting.Startup))]
+
+namespace SelfHosting
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
